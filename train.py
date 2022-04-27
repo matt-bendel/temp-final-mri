dd092d58322fcd16f402b6f61525190774a5c4d1
Matt Bendel <bendel.8@osu.edu>
2022-04-27 16:15:40 -0400

    Added debug plot

--- train.py
+++ train.py
@@ -6,7 +6,9 @@ import pytorch_msssim
 import numpy as np
 import torch.autograd as autograd
 import matplotlib.pyplot as plt
-
+# TODO: REMOVE
+import imageio as iio
+################
 from typing import Optional
 from data import transforms
 from utils.math import complex_abs
@@ -90,6 +92,82 @@ def compute_gradient_penalty(D, real_samples, fake_samples, args, y):
     return gradient_penalty
 
 
+# TODO: REMOVE
+def generate_image(fig, target, image, method, image_ind, rows, cols, kspace=False, disc_num=False):
+    # rows and cols are both previously defined ints
+    ax = fig.add_subplot(rows, cols, image_ind)
+    if method != 'GT' and method != 'Std. Dev':
+        psnr_val = psnr(target, image)
+        snr_val = snr(target, image)
+        ssim_val = ssim(target, image)
+        if not kspace:
+            pred = disc_num
+            ax.set_title(
+                f'PSNR: {psnr_val:.2f}, SNR: {snr_val:.2f}\nSSIM: {ssim_val:.4f}, Pred: {pred * 100:.2f}% True') if disc_num else ax.set_title(
+                f'PSNR: {psnr_val:.2f}, SNR: {snr_val:.2f}\nSSIM: {ssim_val:.4f}')
+
+    if method == 'Std. Dev':
+        im = ax.imshow(image, cmap='viridis')
+        ax.set_xticks([])
+        ax.set_yticks([])
+    else:
+        if kspace:
+            image = image ** 0.4
+            target = target ** 0.4
+        im = ax.imshow(np.abs(image), cmap='gray', vmin=0, vmax=np.max(target))
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_xlabel(method)
+
+    return im, ax
+
+
+def generate_error_map(fig, target, recon, method, image_ind, rows, cols, relative=False, k=1, kspace=False):
+    # Assume rows and cols are available globally
+    # rows and cols are both previously defined ints
+    ax = fig.add_subplot(rows, cols, image_ind)  # Add to subplot
+
+    # Normalize error between target and reconstruction
+    if kspace:
+        recon = recon ** 0.4
+        target = target ** 0.4
+
+    error = (target - recon) if relative else np.abs(target - recon)
+    # normalized_error = error / error.max() if not relative else error
+    if relative:
+        im = ax.imshow(k * error, cmap='bwr', origin='lower', vmin=-0.0001, vmax=0.0001)  # Plot image
+        plt.gca().invert_yaxis()
+    else:
+        im = ax.imshow(k * error, cmap='jet', vmax=1) if kspace else ax.imshow(k * error, cmap='jet', vmax=0.0001)
+
+    # Remove axis ticks
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    # Return plotted image and its axis in the subplot
+    return im, ax
+
+def gif_im(true, gen_im, index, type, disc_num=False):
+    fig = plt.figure()
+
+    generate_image(fig, true, gen_im, f'z {index}', 1, 2, 1, disc_num=False)
+    im, ax = generate_error_map(fig, true, gen_im, f'z {index}', 2, 2, 1)
+
+    plt.savefig(f'/home/bendel.8/Git_Repos/temp-final-mri/gif_{type}_{index - 1}.png')
+    plt.close()
+
+
+def generate_gif(type):
+    images = []
+    for i in range(8):
+        images.append(iio.imread(f'/home/bendel.8/Git_Repos/temp-final-mri/gif_{type}_{i}.png'))
+
+    iio.mimsave(f'variation_gif.gif', images, duration=0.25)
+
+    for i in range(8):
+        os.remove(f'/home/bendel.8/Git_Repos/temp-final-mri/gif_{type}_{i}.png')
+######################
+
 def train(args):
     args.exp_dir.mkdir(parents=True, exist_ok=True)
 
@@ -156,7 +234,7 @@ def train(args):
             std_weight = np.sqrt(2 / (np.pi * args.num_z * (args.num_z + 1)))
             adv_weight = 1e-4
             g_loss = - adv_weight * torch.mean(gen_pred_loss)
-            g_loss += F.l1_loss(avg_recon, x) #- args.ssim_weight * mssim_tensor(x, avg_recon)
+            g_loss += F.l1_loss(avg_recon, x)  # - args.ssim_weight * mssim_tensor(x, avg_recon)
             g_loss += - std_weight * torch.mean(torch.std(gens, dim=1), dim=(0, 1, 2, 3))
 
             g_loss.backward()
@@ -200,14 +278,18 @@ def train(args):
                 gt[:, :, :, :, 1] = x[:, 16:32, :, :]
 
                 for j in range(y.size(0)):
-                    avg_gen_np = transforms.root_sum_of_squares(complex_abs(avg_gen[j] * std[j] + mean[j])).cpu().numpy()
+                    avg_gen_np = transforms.root_sum_of_squares(
+                        complex_abs(avg_gen[j] * std[j] + mean[j])).cpu().numpy()
                     gt_np = transforms.root_sum_of_squares(complex_abs(gt[j] * std[j] + mean[j])).cpu().numpy()
 
                     losses['ssim'].append(ssim(gt_np, avg_gen_np))
                     losses['psnr'].append(psnr(gt_np, avg_gen_np))
 
                     if i == 0 and j == 2:
-                        output = transforms.root_sum_of_squares(complex_abs(avg_gen[0] * std[0] + mean[0])).cpu().numpy()
+                        output = transforms.root_sum_of_squares(
+                            complex_abs(avg_gen[0] * std[0] + mean[0])).cpu().numpy()
+                        target = transforms.root_sum_of_squares(
+                            complex_abs(gt[0] * std[0] + mean[0])).cpu().numpy()
 
                         gen_im_list = []
                         for z in range(args.num_z):
@@ -223,6 +305,13 @@ def train(args):
 
                         std_dev = std_dev / args.num_z
                         std_dev = np.sqrt(std_dev)
+
+                        place = 1
+                        for r, val in enumerate(gen_im_list):
+                            gif_im(target, val, place, 'image')
+                            place += 1
+
+                        generate_gif('image')
 
                         fig = plt.figure()
                         ax = fig.add_subplot(1, 1, 1)
