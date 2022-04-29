07d5c3c767793a9c8defbb0237baa9dfceac56b0
Matt Bendel <bendel.8@osu.edu>
2022-04-29 14:45:10 -0400

    Removed debug logic and added MVUE to validation process instead of RSS

--- evaluation_scripts/cfid/cfid_metric.py
+++ evaluation_scripts/cfid/cfid_metric.py
@@ -7,6 +7,7 @@ import sigpy as sp
 
 from tqdm import tqdm
 from utils.math import tensor_to_complex_np
+from utils.fftc import ifft2c_new, fft2c_new
 
 
 def symmetric_matrix_square_root_torch(mat, eps=1e-10):
@@ -144,7 +145,7 @@ class CFIDMetric:
             reformatted[:, :, :, 0] = multi_coil_inp[i, 0:16, :, :]
             reformatted[:, :, :, 1] = multi_coil_inp[i, 16:32, :, :]
             reformatted = tensor_to_complex_np(
-                (reformatted * std[i, None, None, None] + mean[i, None, None, None]).cpu())  #
+                (fft2c_new(reformatted * std[i, None, None, None] + mean[i, None, None, None])).cpu())  #
 
             im = torch.tensor(self._get_mvue(reformatted.reshape((1,) + reformatted.shape),
                                              sense_maps[i].reshape((1,) + sense_maps[i].shape)))[
